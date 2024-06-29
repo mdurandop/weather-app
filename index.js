031e9d0d04593dbb3d7ba91e5f5618a9d22a9e7b
mdurando <151706154+mdurandop@users.noreply.github.com>
2024-06-29 01:05:15 -0500

    add current time images

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ async function getWeatherData(city) {
 
     console.log(data)
 
+    // Cambiar los datos de la pagina a los obtenidos por la API
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)}°`;
-    document.querySelector('.weather-img').src = `images/weather-imgs/${data.weather[0].main}.png`;
     document.querySelector('.humidity p').innerHTML = `${data.main.humidity}%`;
     document.querySelector('.wind p').innerHTML = `${(data.wind.speed).toFixed(0)} km/h`;
     document.querySelector('.max').innerHTML = `Max: ${Math.floor(data.main.temp_max)}°`;
     document.querySelector('.min').innerHTML = `Min: ${Math.floor(data.main.temp_min)}°`;
+
+    const sunrise = data.sys.sunrise;
+    const sunset = data.sys.sunset;
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    if (currentTime >= sunrise && currentTime < sunset) {
+        document.body.style.background = 'linear-gradient(170deg, rgba(41,178,221,1) 0%, rgba(51,170,221,1) 47%, rgba(45,200,234,1) 100%)';
+        document.querySelector('.weather-img').src = `images/weather-imgs/${data.weather[0].description} light.png`;
+    } else {
+        document.body.style.background = 'linear-gradient(170deg, rgba(8,36,79,1) 0%, rgba(19,76,181,1) 47%, rgba(11,66,171,1) 100%)';
+        document.querySelector('.weather-img').src = `images/weather-imgs/${data.weather[0].description} dark.png`;
+    }
     
 };
 
-getWeatherData('barranquilla');
+getWeatherData('roma');
+
